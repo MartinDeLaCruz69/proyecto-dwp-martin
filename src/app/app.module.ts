d26184b6ea0e2dc770db47f5d03ab7468d597ea0
martindelacruz <martindelacruz169@gmail.com>
2022-10-01 19:58:29 -0500

    Feat: Se agrega el componente nueva contraseña

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NuevacontrasenaComponent } from './nuevacontrasena/nuevacontrasena.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    NuevacontrasenaComponent
   ],
   imports: [
     BrowserModule,
